5cd89521e57b1829dec25865f1ac9e9c02ec8d05
Brendan Weibrecht <zimbico@gmail.com>
2014-12-28 14:18:16 +1100

    Fix Player.test spec

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -6,9 +6,11 @@ describe Player do
     it "can move the player left" do
       player = Player.new
       board = Board.new 3, 3
-      tile_start = Tile.new
+      tile_start = board.new_tile
       board[1,1] = tile_start
       tile_start.place player
+      board[1,1].inhabitants_classes.must_equal [Player]
+      board[0,1].inhabitants_classes.must_equal []
       player.move LEFT
       board[1,1].inhabitants_classes.must_equal []
       board[0,1].inhabitants_classes.must_equal [Player]
